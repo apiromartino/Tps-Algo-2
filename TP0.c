21cdefdd60eeca336cf6cf3a9fbf03346d1d4ae1
Ariii1996 <ariel.piro@hotmail.com>
2016-08-25 11:10:04 -0300

    Update TP0.c

--- TP0.c
+++ TP0.c
@@ -10,12 +10,12 @@ void swap (int* x, int* y){
 
 
 int maximo(int vector[], int n){
-	int pos;
-	int max=0;
 	if (n==0){
 		return -1;
     }	
-	else {for (int i=0; i<n;i++){
+	else {int max=vector[0];
+	      int pos=0;
+	      for (int i=0; i<n;i++){
 		if (vector[i]>max){
 			max=vector[i];
 			pos=i;
@@ -24,13 +24,7 @@ int maximo(int vector[], int n){
 	return pos;
 	}			
 }		
-/* La función comparar recibe dos vectores y sus respectivas longitudes
- * y devuelve -1 si el primer vector es menor que el segundo; 0 si son
- * iguales; y 1 si el segundo es menor.
- *
- * Un vector es menor a otro cuando al compararlos elemento a elemento,
- * el primer elemento en el que difieren no existe o es menor.
- */
+
 int comparar(int vector1[], int n1, int vector2[], int n2){
 
 	for(int i=0;i<n1 && i<n2;i++){	
@@ -52,9 +46,7 @@ int comparar(int vector1[], int n1, int vector2[], int n2){
 		}
 }
 
-/* selection_sort() ordena el arreglo recibido mediante el algoritmo de
- * selección.
- */
+
 void seleccion(int vector[], int n){
 	int aux;
 	for (int i=0;i<n-1;i++){
@@ -66,4 +58,4 @@ void seleccion(int vector[], int n){
 			}	
 	}
 		}
-}	
+}
