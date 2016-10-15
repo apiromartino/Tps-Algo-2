56d98de17db840b0714e3991a93b8984a41a4161
Ariii1996 <ariel.piro@hotmail.com>
2016-10-15 17:38:35 -0300

    Update Hash.c

--- Hash.c
+++ Hash.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 
-#define TAM_INI 97
+#define TAM_INI 337
 
 typedef enum {BORRADO, LLENO, VACIO} estado_t;
 
@@ -24,14 +24,19 @@ struct hash{
 
 
 
-size_t Hash(const char *str, size_t tam){
-    
-    size_t hash = 5381;
-    int c = *str;
-	while (c == *str++)
-        hash = hash * 33 + c; 
-
-    return hash % tam;
+size_t hashing(const char *key, size_t largo){
+    size_t hash, i;
+    for(hash = i = 0; i < strlen(key); ++i){
+    size_t aux = (size_t)(key[i]);
+        hash += aux;
+        hash += (hash << 10);
+        hash ^= (hash >> 6);
+    }
+    hash += (hash << 3);
+    hash ^= (hash >> 11);
+    hash += (hash << 15);
+    hash %= largo;
+    return hash;
 }
 
 
@@ -98,15 +103,15 @@ bool redimensionar_hash (size_t tam_nuevo, hash_t* hash){
 	hash_inicializar(hash, tam_nuevo, hash->destructor);
 	
 	for(int i = 0; i < tam; i++){
-		if (aux[i].estado == LLENO){
-			if (!hash_guardar(hash, aux[i].clave, aux[i].dato)){
-				hash->tam = tam;
-				hash->cantidad = cantidad;
-				hash->cantidad_borrado = cantidad_borrado;
-				hash->tabla = aux;
-				tabla_destruir(tabla_nueva, tam_nuevo);
-				return false;
-			}	
+		if (aux[i].estado != LLENO) continue;
+		
+		if (!hash_guardar(hash, aux[i].clave, aux[i].dato)){
+			hash->tam = tam;
+			hash->cantidad = cantidad;
+			hash->cantidad_borrado = cantidad_borrado;
+			hash->tabla = aux;
+			tabla_destruir(tabla_nueva, tam_nuevo);
+			return false;
 		}
 	}
 
@@ -127,13 +132,14 @@ char *strdup(const char *old) {
 int hash_localizar_clave(const hash_t *hash, const char *clave, size_t* posicion){
 	size_t pos = 0;
 	if (*clave != '\0')
-		pos = Hash(clave, hash->tam);
+		pos = hashing(clave, hash->tam);
+		
 	bool vacio = false;
 	while (!vacio){
 		if (pos >= hash->tam){
 				pos = 0;
 		}
-		else if ((hash->tabla[pos].estado == BORRADO) || (hash->tabla[pos].estado == LLENO && !(strcmp(hash->tabla[pos].clave, clave) == 0))){
+		else if ((hash->tabla[pos].estado == BORRADO) || (hash->tabla[pos].estado == LLENO && strcmp(hash->tabla[pos].clave, clave) != 0)){
 						pos++;
 			}
 			else if (hash->tabla[pos].estado == VACIO){
@@ -149,6 +155,13 @@ int hash_localizar_clave(const hash_t *hash, const char *clave, size_t* posicion
 }
 
 bool hash_guardar(hash_t *hash, const char *clave, void *dato){
+	size_t factor_carga = ((hash->cantidad + hash->cantidad_borrado) * 100) / hash->tam;
+	if (factor_carga >= 72){
+		if(!redimensionar_hash (hash->tam * 2, hash)){
+			return false;
+		}
+	}
+
 	if (clave == NULL){
 		return false;
 	}
@@ -171,17 +184,17 @@ bool hash_guardar(hash_t *hash, const char *clave, void *dato){
 		hash->tabla[pos].dato = dato;
 	}
 
-	size_t factor_carga = ((hash->cantidad + hash->cantidad_borrado) * 100) / hash->tam;
-	if (factor_carga >= 70){
-		if(!redimensionar_hash (hash->tam * 2, hash)){
-			return false;
-		}
-	}
 	return true;
 }
 	
 
 void *hash_borrar(hash_t *hash, const char *clave){
+	
+	size_t factor_carga = ((hash->cantidad * 100)) / hash->tam;
+	if (factor_carga <= 17 && hash->tam > TAM_INI){
+		redimensionar_hash (hash->tam/2, hash);
+	}
+
 	if (clave == NULL){
 		return false;
 	}
@@ -197,14 +210,7 @@ void *hash_borrar(hash_t *hash, const char *clave){
 		hash->cantidad_borrado++;
 	}
 	
-	size_t factor_carga = ((hash->cantidad * 100)) / hash->tam;
-	if (factor_carga <= 15 && hash->tam > TAM_INI){
-		if(!redimensionar_hash (hash->tam/2, hash)){
-			return NULL;
-		}
-	}
-	
-	return hash->tabla[pos].dato; 
+	return hash->tabla[pos].dato;
 }
 	
 void *hash_obtener(const hash_t *hash, const char *clave){
